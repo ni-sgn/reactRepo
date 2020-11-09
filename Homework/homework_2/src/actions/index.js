208e7c687d59d11594672089907411b27ebcd92d
ni-sgn <ni.saganeli@gmail.com>
2020-11-10 00:41:58 +0400

    patching redux

--- Homework/homework_2/src/actions/index.js
+++ Homework/homework_2/src/actions/index.js
@@ -1,21 +1,27 @@
 
-const increment = () =>
+
+
+//I had a bug there, missed 'export' but it didn't pop up as an error
+//which is weird, it made detecting this bug hard...
+//what did App.js even do with increment and decrement without them being exported??? weird
+export const increment = () =>
 {
     return {
         type: 'INCREMENT'
     }
 }
 
-const decrement = () => {
+export const decrement = () => {
     return {
         type: 'DECREMENT'
     }
 }
 
-const addNumber = (number) =>
+export const addNumber = (number) =>
 {
     return {
         type: 'ADD_NUMBER',
         payload: number
     }
 }
+
